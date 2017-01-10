7439321378c4680cbe785888ad796b3dba0241d3
Maxim Aleynikov <max.aleyinikov@gmail.com>
2017-01-10 19:25:29 +0200

    Add command and processer select option.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 
@@ -28,6 +29,19 @@ def random_photo(bot, update):
 def echo(bot, update):
 	update.message.reply_text(update.message.text)
 
+def photo_selection(bot, update):
+	keyboard = [InlineKeyboardButton('Option 1', callback_data=1),
+		InlineKeyboardButton('Option 2', callback_data=2)]
+
+	reply_markup = InlineKeyboardMarkup(keyboard)
+
+	update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+def btn_photo_selection(bot, update):
+	query = update.callback_query
+
+	update.message.reply_text(query.data)
+
 def error(bot, update, error):
 	logger.warn('Update "%s" caused error "%s"' % (update, error))
 
@@ -40,6 +54,8 @@ def main():
 
 	dp.add_handler(MessageHandler([Filters.text], echo))
 	dp.add_handler(CommandHandler('random_girl', random_photo))
+	dp.add_handler(CommandHandler('friday_girls', photo_selection))
+	dp.add_handler(CallbackQueryHandler(btn_photo_selection))
 
 	dp.add_error_handler(error)
 
